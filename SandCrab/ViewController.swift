aaf90f382bb5cb77d5ce5923076a2cbd7a8b2352
Anthony Sherbondy <ts1235813@gmail.com>
2014-09-01 15:33:29 -0700

    initial commit

---
+++ SandCrab/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SandCrab
+//
+//  Created by Anthony Sherbondy on 9/1/14.
+//  Copyright (c) 2014 Sutro Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
