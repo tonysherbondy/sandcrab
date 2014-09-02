3ac9cf59297355b73a30aaec90dda0d73f4e1d32
Anthony Sherbondy <ts1235813@gmail.com>
2014-09-01 17:22:13 -0700

    squash this

---
+++ SandCrab/ChatCell.swift
@@ -0,0 +1,26 @@
+//
+//  ChatCell.swift
+//  SandCrab
+//
+//  Created by Anthony Sherbondy on 9/1/14.
+//  Copyright (c) 2014 Sutro Labs. All rights reserved.
+//
+
+import UIKit
+
+class ChatCell: UITableViewCell {
+
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var chatLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
