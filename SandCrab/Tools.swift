3a9f3bd689990d47e595382884acd80ae46dce7e
drnick23 <nick@halper.de>
2014-09-22 12:30:04 -0700

    Added user login flow and local user persistence

---
+++ SandCrab/Tools.swift
@@ -0,0 +1,13 @@
+//
+//  Tools.swift
+//  SandCrab
+//
+//  Created by Nicolas Halper on 9/22/14.
+//  Copyright (c) 2014 Sutro Labs. All rights reserved.
+//
+
+import UIKit
+
+class Tools: NSObject {
+   
+}
---
+++ SandCrab/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  SandCrab
+//
+//  Created by Nicolas Halper on 9/22/14.
+//  Copyright (c) 2014 Sutro Labs. All rights reserved.
+//
+
+import UIKit
+
+class User: NSObject {
+   
+}
